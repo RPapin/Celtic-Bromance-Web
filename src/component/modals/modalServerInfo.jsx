0bab4d361cafb96a72c1836d88f003ea30467928
RPapin <remi.papin@viacesi.fr>
2021-11-01 17:21:57 +0100

    aa

---
+++ src/component/modals/modalServerInfo.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button';
+import ReadData from '../../services/readData'
+import Modal from 'react-bootstrap/Modal';
+import './modalServerInfo.css'
+import Form from 'react-bootstrap/Form'
+import { useCookies } from 'react-cookie';
+
+
+const ModalServerInfo = ({setModalInfo}) => {
+  const readData = new ReadData()
+  // const [show, setShow] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [inputList, setInputList] = useState([])
+
+
+  const handleClose = () => {
+
+    setModalInfo(false);
+  }
+
+  const getWeatherPct = (weatherWeight) => {
+    let total = 0;
+    let weatherPct = []
+    weatherWeight.forEach(element => {
+      total += element
+    });
+    weatherWeight.forEach(element => {
+      let pct = Math.round(element / total * 100)
+      weatherPct.push(pct)
+    });
+    return weatherPct
+  }
+  const fetchServerInfo = async () => {
+  
+    
+    let paramFromApi = await readData.getLocalApi("get_param_list")
+    //For testing purpose
+    let list = []
+    Object.keys(paramFromApi['paramList']).map((fileName) => {
+      paramFromApi['paramList'][fileName].map((param, i) => {
+        list[param.name] = param.currentValue
+        if(param.name == 'weatherWeightConfiguration'){
+          list['weatherPct'] = getWeatherPct(param.currentValue)
+        }
+    })
+    setInputList(list);
+    setLoading(false)
+    })
+  }
+
+  useEffect( () => {
+    if(loading)fetchServerInfo()
+  })
+  return (
+    <>
+    {!loading && 
+      <Modal show={true} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Server Informations</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="info-title">Point distribution : </div>
+              <div>{inputList["pointConfiguration"].map( (point) => {
+                  return point + ','
+              })}</div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <div className="info-title">Weather probability : </div>
+              <div>Flooded => {inputList["weatherPct"][0]}% , Wet => {inputList["weatherPct"][1]}% , Damp => {inputList["weatherPct"][2]}% , Dry => {inputList["weatherPct"][3]}% </div>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose} >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    }
+    </>
+  );
+}
+
+export default ModalServerInfo
